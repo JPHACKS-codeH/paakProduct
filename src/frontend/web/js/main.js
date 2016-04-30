9b0bfa8925e24f040997140b0cdf0b797e0e158c
UekusaTomoki <tmkueks19@gmail.com>
2016-04-30 15:36:14 +0900

    Implementation ver1.0

--- src/frontend/web/js/main.js
+++ src/frontend/web/js/main.js
@@ -1,81 +1,93 @@
 var vueObj;
 $(document).ready(function(){
-  // var vueObj = new Vue({
-  //   el: '#vuetest',
-  //   data: {
-  //     array_data:[]
-  //   }
-  // });
-
-  // TODO ajaxテスト
-  // $.ajax({
-  //   url: 'http://133.25.210.30:34001/hashtag',
-  //   type: 'POST',
-  //   dataType: 'json',
-  //   data: {param1: 'test'}
-  // })
-  // .done(function() {
-  //   console.log("success");
-  // })
-  // .fail(function() {
-  //   console.log("error");
-  // })
-  // .always(function() {
-  //   console.log("complete");
-  // });
-
   vueObj = new Vue({
-    el: '#upload_form',
+    el: 'main',
     data: {
       hash_tags:[],
-      text_message:""
+      text_message:"",
+      view_section:false,
+      hash_loading:false
     }
   });
-  vueObj.text_message = "今日もうちの猫は可愛かった。最高。";
-
-  // var timer = setTimeout(function(){
-  //   vueObj.array_data = ["hello","world","!!","good!!"]
-  // },1000);
-  $('#upload_img').uploadThumbs({
+  $('#up_file').uploadThumbs({
         position : '#preview1'
-    });
+  });
+  $("#bgndVideo").YTPlayer();
+  $('#background_video').tubular({
+    videoId: '4Bh1nm7Ir8c',
+    repeat: true,
+    mute: true
+  });
 });
-function upload(){
+/**
+ * 選択した画像をajaxを利用してアップロード
+ * @author tomoki uekusa
+ */
+function upload(form){
+  var tmp_str = $("#txt_area").val()
+  for (var i = 0; i < vueObj.hash_tags.length; i++) {
+    tmp_str += " "+vueObj.hash_tags[i];
+  }
+  $("#txt_area").val(tmp_str);
+
   $form = $('#upload_form');
-  $img = $('#upload_img');
-  fd = new FormData($form);
-  console.log($('#upload_img').val());
-  // var body = { "Url": "http://www.chihirophoto.com/pets/images/pets%20top-1.jpg"};
-  // var body = {"Url": $img[0].value};
+  var fd = new FormData($form[0]);
+
   $.ajax({
-    url: "https://api.projectoxford.ai/vision/v1/analyses?visualFeatures=All",
-    beforeSend: function(xhrObj){
-      xhrObj.setRequestHeader("Content-Type","application/json");
-      xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","818614e1a0f54568b370bfe0cdba54cf");
-    },
-    type: "POST",
+    url: "http://133.25.210.30:34001/oauth",
+    type: "post",
     processData: false,
     contentType: false,
-    // data: JSON.stringify(body),
-    data: {
-      'file': $('#upload_img').val()
-    },
+    dataType: 'json',
+    data: fd,
     success: function(data) {
-        // alert( data.message );
+      console.log("success");
+      console.log(data.message);
+      console.log(data);
+      window.location.href = data;
+    },
+    error: function(XMLHttpRequest, textStatus, errorThrown) {
+      console.log("error");
+      console.log(textStatus);
+      console.log(errorThrown);
+
+    }
+  });
+}
+/**
+ * @param
+ * @return
+ */
+function getTextarea(text){
+  // console.log("text is changed");
+  // vueObj.text_message = text.value;
+}
+function clickTag(tag_index){
+  vueObj.hash_tags.splice( tag_index, 1 );
+}
+function changeImage(form){
+  vueObj.view_section = true;
+  vueObj.hash_loading = true;
+  $form = $('#upload_form');
+  var fd = new FormData($form[0]);
+  console.log(form[0]);
+  console.log(fd);
+  $.ajax({
+    url: "http://133.25.210.30:34001/hashtag",
+    type: "post",
+    processData: false,
+    contentType: false,
+    dataType: 'json',
+    data: fd,
+    success: function(data) {
         console.log(data.message);
         console.log(data);
+        vueObj.hash_tags = data.hashtags;
+        vueObj.hash_loading = false;
     },
     error: function(XMLHttpRequest, textStatus, errorThrown) {
         console.log(textStatus);
         console.log(errorThrown);
-        // alert( "ERROR" );
-        // alert( textStatus );
-        // alert( errorThrown );
     }
   });
-  setTimeout(function(){
-    console.log("created hash");
-    vueObj.hash_tags = ["猫","cat","動物","かわいい"];
-  },1000);
-  return false;
 }
